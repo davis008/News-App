0398476d635a517762ef03a5f14b2cdf972019a0
davis008 <davisomondi008@gmail.com>
2021-02-10 11:57:10 +0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDom.render(
+    <h1>hey</h1>,
+
+    document.getElementById('root')
+
+);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path=require('path');
+
+
+module.exports={
+    entry:'./src/app.tsx',
+    devtool: 'inline-source-map',
+    module:{
+        rules:[
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve:{
+        extensions:['.tsx','.ts','.js'],
+    },
+    output:{
+        filename: 'build/app.js',
+        path: path.resolve(__dirname,'dist'),
+    },
+
+};
